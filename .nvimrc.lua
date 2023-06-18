b3dcffc591666b18198c613c2f720b867b95efda
niveK77pur <niveK77pur@users.noreply.github.com>
2023-06-18 09:43:46 +0000

    Apply automatic changes

--- .nvimrc.lua
+++
@@ -1,8 +0,0 @@
-require("nvls").setup({
-    lilypond = {
-        options = {
-            main_file = "Sound-and-Vision.ly",
-            include_dir = "./openlilylib",
-        },
-    },
-})
--- README.md
+++
@@ -1,7 +0,0 @@
-# About
-
-[Lilypond](https://lilypond.org/) source code for arrangement.
-
-# Recording
-
-- <https://youtu.be/0IltdHJ3TjU>
